206feaf409ea696de376c84c0bd895f75c2d9f30
Kap-taan <78801549+Kap-taan@users.noreply.github.com>
2021-05-08 14:12:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const Todolistform = document.querySelector(".todolists");
 
 Todobutton.addEventListener('click', Add);
 Todolistform.addEventListener('click', ComDelete);
+document.addEventListener("DOMContentLoaded", getTodos);
 
 // Functions
 function Add(event){
@@ -18,6 +19,7 @@ function Add(event){
     let Todoli = document.createElement('li');
     Todoli.classList.add('todoli');
     Todoli.innerText = Todoinput.value;
+    saveLocalTodos(Todoinput.value);
     Tododiv.appendChild(Todoli);
     Todoinput.value = "";
     let Completebutton = document.createElement('button');
@@ -37,6 +39,7 @@ function ComDelete(event){
     if(item.classList[0] === "destroybtn"){
         const paradiv = item.parentElement;
         paradiv.classList.add('fall');
+        removeLocalTodos(paradiv);
         paradiv.addEventListener("transitionend", event => {
             paradiv.remove();
         })
@@ -48,3 +51,60 @@ function ComDelete(event){
         
     }
 }
+
+
+// 
+
+function saveLocalTodos(todo) {
+    let todos;
+    if (localStorage.getItem("todos") === null) {
+      todos = [];
+    } else {
+      todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
+  function removeLocalTodos(todo) {
+    let todos;
+    if (localStorage.getItem("todos") === null) {
+      todos = [];
+    } else {
+      todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    const todoIndex = todo.children[0].innerText;
+    todos.splice(todos.indexOf(todoIndex), 1);
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
+  
+  function getTodos() {
+    let todos;
+    if (localStorage.getItem("todos") === null) {
+      todos = [];
+    } else {
+      todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    todos.forEach(function(todo) {
+      //Create todo div
+      const todoDiv = document.createElement("div");
+      todoDiv.classList.add("tododiv");
+      //Create list
+      const newTodo = document.createElement("li");
+      newTodo.innerText = todo;
+      newTodo.classList.add("todoli");
+      todoDiv.appendChild(newTodo);
+      Todoinput.value = "";
+      //Create Completed Button
+      const completedButton = document.createElement("button");
+      completedButton.innerHTML = `<i class="fas fa-check"></i>`;
+      completedButton.classList.add("completebtn");
+      todoDiv.appendChild(completedButton);
+      //Create trash button
+      const trashButton = document.createElement("button");
+      trashButton.innerHTML = `<i class="fas fa-trash"></i>`;
+      trashButton.classList.add("destroybtn");
+      todoDiv.appendChild(trashButton);
+      //attach final Todo
+      Todolistform.appendChild(todoDiv);
+    });
+  }
